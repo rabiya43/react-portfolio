4a2ce481508d6a0d4b4e1727a6c26652bf81012d
rabiya43 <bsai23043@itu.edu.pk>
2025-05-04 19:45:36 +0500

    fixed errors and make changes in sidenav pages and made sure no error is undetected

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
